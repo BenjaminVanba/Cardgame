f05f077fbb30022625e3523517d0c30f31eaeb47
Vanbaelinghem <benjamin.vanbaelinghem@epitech.eu>
2024-11-05 12:24:23 +0100

    coucou

--- Interface.java
+++ Interface.java
@@ -1,5 +1,9 @@
 public class Interface {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        System.out.println("Hello, prout!");
+
+        System.out.println("Hello, rachid!");
+
     }
 }
