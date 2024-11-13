eef8d28576b3a99278584bc911e7c50423e30435
Vanbaelinghem <benjamin.vanbaelinghem@epitech.eu>
2024-11-13 14:47:55 +0100

    ajout des cartes

--- libGDXtest/core/src/main/java/io/github/blackjack/Main.java
+++ libGDXtest/core/src/main/java/io/github/blackjack/Main.java
@@ -3,6 +3,8 @@ package io.github.blackjack;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.Stage;
@@ -14,16 +16,22 @@ import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
 import com.badlogic.gdx.utils.ScreenUtils;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 
-/** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
 public class Main extends ApplicationAdapter {
     private Stage stage;
     private Skin skin;
+    private Texture backgroundTexture; // Texture pour l'arrière-plan
+    private SpriteBatch batch;
 
     @Override
     public void create() {
+        // Initialiser le SpriteBatch et charger la texture
+        batch = new SpriteBatch();
+        backgroundTexture = new Texture(Gdx.files.internal("Background.jpg"));
+
         stage = new Stage(new FitViewport(640, 480));
         skin = new Skin(Gdx.files.internal("ui/uiskin.json"));
 
+        // Configuration de la fenêtre d'interface utilisateur
         Window window = new Window("Example screen", skin, "border");
         window.defaults().pad(4f);
         window.add("This is a simple Scene2D view.").row();
@@ -37,10 +45,8 @@ public class Main extends ApplicationAdapter {
         });
         window.add(button);
         window.pack();
-        // We round the window position to avoid awkward half-pixel artifacts.
-        // Casting using (int) would also work.
         window.setPosition(MathUtils.roundPositive(stage.getWidth() / 2f - window.getWidth() / 2f),
-            MathUtils.roundPositive(stage.getHeight() / 2f - window.getHeight() / 2f));
+                MathUtils.roundPositive(stage.getHeight() / 2f - window.getHeight() / 2f));
         window.addAction(Actions.sequence(Actions.alpha(0f), Actions.fadeIn(1f)));
         stage.addActor(window);
 
@@ -49,7 +55,14 @@ public class Main extends ApplicationAdapter {
 
     @Override
     public void render() {
+        // Efface l'écran et dessine l'arrière-plan
         ScreenUtils.clear(0f, 0f, 0f, 1f);
+
+        batch.begin();
+        batch.draw(backgroundTexture, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        batch.end();
+
+        // Dessine la scène
         stage.act(Gdx.graphics.getDeltaTime());
         stage.draw();
     }
@@ -63,5 +76,7 @@ public class Main extends ApplicationAdapter {
     public void dispose() {
         stage.dispose();
         skin.dispose();
+        batch.dispose(); // Libère le SpriteBatch
+        backgroundTexture.dispose(); // Libère la texture
     }
 }
