51ce67841aa8589ddd62d910092b2a6f02cbdd7a
rhtrplx <rachid.lamkadem@epitech.eu>
2024-11-14 11:17:07 +0100

    wip classes

--- libGDXtest/core/src/main/java/io/github/blackjack/Card.java
+++ libGDXtest/core/src/main/java/io/github/blackjack/Card.java
@@ -2,35 +2,58 @@ package io.github.blackjack;
 
 import java.util.Random;
 
-public class Card implements ICard {
-    protected String value;
-    protected String type;
+public class Card {
+    protected int value;
+    protected String rank;
+    protected String suit;
 
     private static final String[] SUITS = { "Cœurs", "Carreaux", "Trèfles", "Piques" };
     private static final String[] RANKS = { "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A" };
+    private static final int[] VALUES = { 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14 };
 
     public Card() {
         Random random = new Random();
-        this.type = SUITS[random.nextInt(SUITS.length)];
-        this.value = RANKS[random.nextInt(RANKS.length)];
+        this.value = VALUES[random.nextInt(VALUES.length)];
+        this.rank = RANKS[random.nextInt(RANKS.length)];
+        this.suit = SUITS[random.nextInt(SUITS.length)];
     }
 
-    public Card(String value, String type) {
+    public Card(int value, String rank, String suit) {
         this.value = value;
-        this.type = type;
+        this.rank = rank;
+        this.suit = suit;
     }
 
     public void DisplayInfo(Card card) {
-        System.out.println("* " + card.getValue() + " de " + card.getType() + " *");
+        System.out.println("* " + card.getValue() + " de " + card.getSuit() + " *");
     }
 
-    @Override
-    public String getValue() {
+    public String getRank() {
+        return this.rank;
+    }
+
+    public int getValue() {
         return this.value;
     }
 
-    @Override
-    public String getType() {
-        return this.type;
+    public String getSuit() {
+        return this.suit;
+    }
+
+    public boolean isAce() {
+        if (this.value == 14) {
+            return true;
+        } else {
+            return false;
+        }
+
+    }
+
+    public boolean isHead() {
+        if (this.value >= 10 && this.value != 14) {
+            return true;
+        } else {
+            return false;
+        }
     }
 }
--- libGDXtest/core/src/main/java/io/github/blackjack/Hand.java
+++ libGDXtest/core/src/main/java/io/github/blackjack/Hand.java
@@ -1,7 +1,53 @@
 package io.github.blackjack;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
-public abstract class Hand {
-    protected ArrayList<Card> cards;
+public class Hand {
+    protected ArrayList<Card> cards = new ArrayList<>();
+
+    private List<Integer> scores = new ArrayList<>(Arrays.asList(0, 0)); // Liste modifiable pour stocker deux valeurs
+                                                                         // de score
+
+    public void DisplayInfo() {
+        // TODO Auto-generated method stub
+        // doit loop dans les cartes et écrire leur info
+    }
+
+    public List<Integer> getValue() {
+        return this.scores;
+    }
+
+    public ArrayList<Card> getCards() {
+        return this.cards;
+    }
+
+    public void addCard(Card card) {
+        this.cards.add(card);
+
+        // Si la carte ajoutée est un As
+        if (card.isAce()) {
+            if (cards.size() == 1) { // Vérifie si c'est la première carte de la main
+                scores.set(0, 1); // Compte l'As comme 11 pour le premier score
+                scores.set(1, 11); // Compte également l'As comme 11 pour le deuxième score
+            } else {
+                scores.set(0, scores.get(0) + 1);
+                scores.set(1, scores.get(1) + 1);
+            }
+        } else {
+            int cardValue = card.getValue(); // Récupère la valeur de la carte
+            scores.set(0, scores.get(0) + cardValue); // Ajoute la valeur au premier score
+            scores.set(1, scores.get(1) + cardValue); // Ajoute la valeur au deuxième score
+        }
+    }
+
+    public boolean isBlackJack() {
+        if (cards.size() == 2) { // Vérifie qu'il y a exactement deux cartes
+            Card firstCard = cards.get(0);
+            Card secondCard = cards.get(1);
+            return firstCard.isAce() && secondCard.isHead();
+        }
+        return false;
+    }
 }
