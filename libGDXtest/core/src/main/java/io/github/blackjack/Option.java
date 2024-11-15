dbfec8a550873b4cdee82cf80f8ef7d83d9e4c8d
Vanbaelinghem <benjamin.vanbaelinghem@epitech.eu>
2024-11-15 10:48:48 +0100

    wip

--- libGDXtest/core/src/main/java/io/github/blackjack/Option.java
+++ libGDXtest/core/src/main/java/io/github/blackjack/Option.java
@@ -1,14 +1,11 @@
 package io.github.blackjack;
 
-import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.Window;
-import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
-import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 
 public class Option extends Window {
-
     private TextButton returnButton;
 
     public Option(Skin skin) {
@@ -18,19 +15,12 @@ public class Option extends Window {
 
     private void setupOptions(Skin skin) {
         Label congratulationsLabel = new Label("Félicitations, ceci est le menu option", skin);
-        add(congratulationsLabel).row(); // Ajouter le texte à la fenêtre
-
         returnButton = new TextButton("Retour", skin);
-        returnButton.addListener(new ChangeListener() {
-            @Override
-            public void changed(ChangeEvent event, Actor actor) {
-                setVisible(false); // Masquer la fenêtre des options
-            }
-        });
 
-        add(returnButton).row();
+        add(congratulationsLabel).row();
+        add(returnButton).padTop(10f).row();
 
-        pack(); // Adapter la taille de la fenêtre au contenu
+        pack();
     }
 
     public TextButton getReturnButton() {
