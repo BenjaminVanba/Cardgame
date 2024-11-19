5f4766cf9c6e9476fded76a0ceb05d791600865e
Vanbaelinghem <benjamin.vanbaelinghem@epitech.eu>
2024-11-19 10:45:57 +0100

    gdx

--- libGDXtest/core/src/main/java/io/github/blackjack/Main.java
+++ libGDXtest/core/src/main/java/io/github/blackjack/Main.java
@@ -1,58 +1,24 @@
 package io.github.blackjack;
 
-import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
-import com.badlogic.gdx.utils.ScreenUtils;
-import com.badlogic.gdx.utils.viewport.FitViewport;
 
-public class Main extends ApplicationAdapter {
-    private Stage stage;
+public class Main extends Game {
     private Skin skin;
-    private Texture backgroundTexture;
-    private SpriteBatch batch;
-    private MainComposite mainComposite;
+    // private Settings settings;
 
     @Override
     public void create() {
-        batch = new SpriteBatch();
-        backgroundTexture = new Texture("Background.jpg");
-
-        stage = new Stage(new FitViewport(800, 480));
-        skin = new Skin(Gdx.files.internal("ui/uiskin.json"));
-
-        // Initialiser MainComposite avec le skin
-        mainComposite = new MainComposite(skin);
-        mainComposite.render(stage); // Ajouter le composite au stage
-
-        Gdx.input.setInputProcessor(stage);
+        this.skin = new Skin(Gdx.files.internal("ui/uiskin.json"));
+        setScreen(new MenuScreen(this, this.skin));
     }
 
-    @Override
-    public void render() {
-        ScreenUtils.clear(0f, 0f, 0f, 1f);
-
-        batch.begin();
-        batch.draw(backgroundTexture, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
-        batch.end();
-
-        stage.act(Gdx.graphics.getDeltaTime());
-        stage.draw();
+    public void showGameScreen() {
+        setScreen(new GameScreen(this, this.skin)); // , settings));
     }
 
-    @Override
-    public void resize(int width, int height) {
-        stage.getViewport().update(width, height);
-    }
-
-    @Override
-    public void dispose() {
-        stage.dispose();
-        skin.dispose();
-        batch.dispose();
-        backgroundTexture.dispose();
-    }
+    // public void setSettings(Settings settings) {
+    // this.settings = settings;
+    // }
 }
